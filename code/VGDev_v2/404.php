4f3cfbd97c266f38809b7d2e0347b52273e57569
Brian Abbott <bsabbott92@gmail.com>
2016-09-11 13:59:43 -0400

    added some documents, image, etc to the current website design

---
+++ code/VGDev_v2/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+		<!-- content -->
+		<div class="container" id="content">
+			<div class="wrapper">
+
+				<p>I am sorry, but this page does not exist...</p>
+
+			</div>
+		</div>
+		<!-- / content -->
+
+<?php get_footer(); ?>
